023bebc6d4943e945f1e91001ff822f9d4f7a8b0
Rajkumar <rajkumar@authenticate.com>
2025-04-08 10:27:16 +0530

    updated cors

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ const app = express();
 app.use(
     cors({
         origin: [
-            "https://job-portal-frontend-eta-two.vercel.app/sign-in",
+            "https://job-portal-frontend-eta-two.vercel.app",
             "http://localhost:5173",
         ],
         credentials: true,
